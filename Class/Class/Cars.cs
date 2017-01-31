f2366f3475d2dff240a6874afc37ac0a1c9aae0c
DESKTOP-G54U40T\Vatol <Vesex2017@gmail.com>
2017-01-31 19:22:00 +0200

    New

---
+++ Class/Class/Cars.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Cars
+    {
+
+       private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public void Say()
+        {
+            Console.WriteLine("I Made in Germay");
+
+
+        }
+
+
+        }
+    }
+
---
+++ HomeCalculator/HomeCalculator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                double x1, x2;
+                string _operator;
+                Console.WriteLine("Enter the first number");
+                x1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter the operator");
+                _operator = Console.ReadLine();
+                Console.WriteLine("Enter the second number");
+                x2 = Convert.ToDouble(Console.ReadLine());
+                switch (_operator)
+                
+                {
+                    case "+":
+                        Console.WriteLine(x1 + x2);
+                       break;
+                       case "-":
+                        Console.WriteLine(x1 - x2);
+                        break;
+                    case "/":
+                        Console.WriteLine(x1 / x2);
+                        break;
+                    case "*":
+                        Console.WriteLine(x1 * x2);
+                        break;
+
+                       }
+
+            }
+
+        }
+    }
+}
